6820c9de4f6b560c235766102588d33b2aaeec8c
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-09-10 19:05:06 -0300

    Adds background and parallax

---
+++ Workers/Assets/Parallax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPos;
+    [SerializeField] private GameObject cam;
+    [SerializeField] private float parallaxAmount;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movedFromCamera = cam.transform.position.x * (1 - parallaxAmount);
+        float dist = cam.transform.position.x * parallaxAmount;
+        transform.position = new Vector3(startPos + dist * 40f * Time.fixedDeltaTime, transform.position.y, transform.position.z);
+
+        if (movedFromCamera > startPos + length)
+            startPos += length;
+        else if (movedFromCamera < startPos - length)
+            startPos -= length;
+    }
+}
