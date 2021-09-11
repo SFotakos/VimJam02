0f7da6d7bde8c2c8b0b30aceb103f2d06ec310e4
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-09-11 16:47:38 -0300

    Fixes edge case jump not landing

--- Workers/Assets/Player/PlayerController.cs
+++ Workers/Assets/Player/PlayerController.cs
@@ -16,15 +16,18 @@ public class PlayerController : MonoBehaviour
     [Range(200f, 800f)] [SerializeField] private float jumpForce = 400f;            // Amount of force added when the player jumps.
     [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;        // How much to smooth out the movement.
 
+    private float horizontalMovement = 0f;
+    private float verticalMovement = 0f;
+    private bool shouldJump = false;
+
     [Header("Jump Handling Parameters")]
     [Space(5)]
     [SerializeField] private LayerMask whatIsGround;                                // A mask determining what is ground to the character.
     [SerializeField] private Transform groundCheck;                                 // A position marking where to check if the player is grounded.
-    [Range(.03f, .15f)] [SerializeField] private float groundCheckDistance = .035f; // Radius of the overlap circle to determine if grounded.
-    private bool isGrounded = true;                                                        // Whether or not the player is grounded.
+    [Range(.03f, .15f)] [SerializeField] private float groundCheckRadius = .035f;   // Radius of the overlap circle to determine if grounded.
+    private bool isGrounded = true;                                                 // Whether or not the player is grounded.
     private Coroutine keepGroundedCoroutine = null;
     private float keepGroundedTime = .08f;                                          // So the play can jump a few milisseconds after he left the ground.
-    public System.Action groundedCallback;
 
     [Header("Ladder Handling Parameters")]
     [Space(5)]
@@ -42,37 +45,40 @@ public class PlayerController : MonoBehaviour
         originalGravityScale = playerRigidbody.gravityScale;
     }
 
-    private void Update()
+    void Update()
     {
         if (playerCombat.hasSnapped)
             return;
 
-        // Keep grounded for a few milisseconds
-        if (playerRigidbody.velocity.y != 0 && keepGroundedCoroutine == null)
-            keepGroundedCoroutine = StartCoroutine(KeepGrounded());
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        // The player is grounded if a raycast to the groundcheck position hits anything designated as ground
-        RaycastHit2D groundHitInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-        if (!isGrounded)
-        {
-            if (keepGroundedCoroutine != null)
-            {
-                StopCoroutine(keepGroundedCoroutine);
-                keepGroundedCoroutine = null;
-            }
-            if (groundHitInfo.collider != null)
-                isGrounded = true;
-        }
+        verticalMovement = Mathf.Max(0, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetButtonDown("Jump") && !animator.GetBool("IsJumping"))
+            shouldJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerCombat.hasSnapped)
+            return;
+
+        // The player is grounded if a raycast from the groundcheck position hits anything designated as ground.
+        Collider2D groundHit = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (groundHit != null && playerRigidbody.velocity.y <= 0f)
+            isGrounded = true;
+        else if (playerRigidbody.velocity.y < 0 && keepGroundedCoroutine == null) 
+            keepGroundedCoroutine = StartCoroutine(KeepGrounded()); // Keep grounded for a few milisseconds
 
         RaycastHit2D ladderHitInfo = Physics2D.Raycast(ladderCheck.position, Vector2.up, ladderCheckDistance, whatIsLadder);
         isNearLadder = ladderHitInfo.collider != null;
 
-        if (isGrounded && playerRigidbody.velocity.y < 0f && animator.GetBool("IsJumping"))
-            groundedCallback();
+        Move(horizontalMovement * Time.fixedDeltaTime, verticalMovement * Time.fixedDeltaTime, shouldJump);
+        shouldJump = false;
 
         if (Debug.isDebugBuild)
         {
-            Debug.DrawLine(groundCheck.position, (Vector2)groundCheck.position + Vector2.down * groundCheckDistance, Color.blue);
+            Debug.DrawLine(groundCheck.position, (Vector2)groundCheck.position + Vector2.down * groundCheckRadius, Color.blue);
             Debug.DrawLine(ladderCheck.position, (Vector2)ladderCheck.position + Vector2.up * ladderCheckDistance, Color.red);
         }
     }
@@ -123,6 +129,7 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
         if (jumping) animator.SetBool("IsJumping", true);
         animator.SetBool("IsClimbing", climbing);
+        if (isGrounded) animator.SetBool("IsJumping", false);
 
         //Flip player
         if (horizontalMovement > 0 && !facingRight) Flip();
