65209d5ba5abc1492bf64a5deafcf812c84200ac
Spyridion Georges Fotakos <s.fotakos@gmail.com>
2021-09-19 17:58:54 -0300

    Fixes credit links and adds transition on credit end

---
+++ Workers/Assets/UI/CreditsEndHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsEndHandler : MonoBehaviour
+{
+    void OnCreditsEnd()
+    {
+        SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+    }
+}
